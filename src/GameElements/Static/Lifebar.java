82afe8f820a1cfbd9e3624fb923bb37e40c7fd48
vb-03 <47537857+vb-03@users.noreply.github.com>
2022-11-29 22:25:39 +0000

    Refactor color variable

--- src/GameElements/Static/Lifebar.java
+++ src/GameElements/Static/Lifebar.java
@@ -19,16 +19,16 @@ public class Lifebar extends GameElement {
 	
 	public void update() {
 		GameEngine.getInstance().clearLifebar();
-		String colorado = "";
+		String tileColor = "";
 		for(int i=0;i<5;i++) {
 			if(i < (LivingElement.getHitpoints()-1)*0.5) {
-				colorado = "Green";
+				tileColor = "Green";
 			} else if (i > (LivingElement.getHitpoints()-1)*0.5){
-				colorado = "Red";
-			} else colorado = "GreenRed";
+				tileColor = "Red";
+			} else tileColor = "GreenRed";
 			Point2D tile_pos = new Point2D(i,getPosition().getY()); //new Point2D(i,GameEngine.GRID_HEIGHT);
-			LifeTile lf = new LifeTile(Color.valueOf(colorado),tile_pos);
-			//GameEngine.getInstance().addObject(GameElement.create(colorado,tile_pos,null,null,null));	
+			LifeTile lf = new LifeTile(Color.valueOf(tileColor),tile_pos);
+			//GameEngine.getInstance().addObject(GameElement.create(tileColor,tile_pos,null,null,null));	
 			GameEngine.getInstance().addObject((GameElement)lf);
 		}
 		if(LivingElement.isDeadOnNextAttack())
